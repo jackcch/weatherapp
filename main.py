2feca1ce3d975508c0faac6a3b251f71390d713c
jackcch <88955228+jackcch@users.noreply.github.com>
2022-08-28 09:47:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 parameters = {
     "lat": "1.340420",
     "lon": "103.879560",
-    "appid": "4329ba1cb6d5dba098ad644a11286430",
+    "appid": "",
 }
 
 response = requests.get(url="https://api.openweathermap.org/data/3.0/onecall", params=parameters)
